8a60824f93e5e88ad1273c6864c9451039cf8856
kailash360 <kailashkejriwal21@gmail.com>
2022-06-01 11:57:04 +0530

    Configured tailwind with mui for frontend

---
+++ frontend/tailwind.config.js
@@ -0,0 +1,14 @@
+module.exports = {
+  content: [
+    "./src/*.{js,jsx,ts,tsx}",
+    "./src/**/*.{js,jsx,ts,tsx}"
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+  corePlugins: {
+    preflight: false,
+  },
+  important: '#root'
+}
