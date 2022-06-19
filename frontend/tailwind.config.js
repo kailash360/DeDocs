677c909cf5c9a7347d8cc1a033c620fb1caf0c25
kailash360 <kailashkejriwal21@gmail.com>
2022-06-19 21:30:38 +0530

    added tailwind confg

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -1,14 +1,21 @@
 module.exports = {
-  content: [
-    "./src/*.{js,jsx,ts,tsx}",
-    "./src/**/*.{js,jsx,ts,tsx}"
-  ],
-  theme: {
-    extend: {},
-  },
-  plugins: [],
-  corePlugins: {
-    preflight: false,
-  },
-  important: '#root'
+    content: [
+        "./src/*.{js,jsx,ts,tsx}",
+        "./src/**/*.{js,jsx,ts,tsx}"
+    ],
+    theme: {
+        extend: {},
+        colors: {
+            primary: '#1B262C',
+            secondary: '#0F4C75',
+            tertiary: '#3282B8',
+            white: '#fff'
+        }
+    },
+    plugins: [],
+    corePlugins: {
+        preflight: false,
+    },
+    important: '#root'
+
 }
