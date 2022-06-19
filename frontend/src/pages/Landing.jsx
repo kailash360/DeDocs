058b4b3948e5299bcca38790646769adcb4d2c9f
kailash360 <kailashkejriwal21@gmail.com>
2022-06-19 21:31:03 +0530

    added landing page

---
+++ frontend/src/pages/Landing.jsx
@@ -0,0 +1,34 @@
+import React from 'react'
+import {Container, Grid, Button} from '@mui/material'
+import {ReactComponent as LandingVector} from '../static/media/LandingVector.svg'
+import '../static/scss/Landing.scss'
+
+function Home() {
+  
+  return (
+    <Grid container overflow-hidden	>
+      <Grid item lg={6} className='min-h-full pl-24 min-h-screen'>
+        <p className='text-6xl text-secondary mx-2 font-bold mb-1'> DeDocs </p>
+        <p className='text-4xl text-secondary mx-2 font-semibold'>
+          A Decentralized <br /> 
+          Document Management <br /> 
+          System 
+        </p>
+        <p className='max-w-lg mx-2'>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Saepe iure laboriosam inventore quisquam omnis quam, dolore quaerat in assumenda dolorem illo aperiam sit aliquam architecto facere adipisci enim incidunt pariatur.
+        </p>
+        <Container maxWidth={false} className="pl-0 flex-row justify-between mt-16">
+          <Button className="bg-secondary text-white normal-case px-8 py-2 text-lg ml-2 mr-8 my-2	">I am a User</Button>
+          <Button className="bg-tertiary text-white normal-case	px-8 py-2 text-lg	">I am an Admin</Button>
+        </Container>
+      </Grid>
+      <Grid item lg={6} className='bg-tertiary min-h-screen'>
+        <LandingVector className="my-[20%]"></LandingVector>
+      </Grid>
+    </Grid>
+  )
+}
+
+export default Home
+
+
