b10e2beafbb4f9929e7b3719e440f72d380440a2
kailash360 <kailashkejriwal21@gmail.com>
2022-06-23 02:27:07 +0530

    added reg box for admin

--- frontend/src/components/Admin/RegistrationBox.jsx
+++ frontend/src/components/Admin/RegistrationBox.jsx
@@ -13,8 +13,8 @@ function RegistrationBox() {
   ]
 
   return (
-    <Container maxWidth={false} className="min-w-full flex flex-col justify-evenly">
-        <p className='text-3xl font-bold'>Register as an Admin</p>
+    <Container maxWidth={false} className="landing-registration">
+        <p className='landing-registration-heading'>Register as an Admin</p>
         <Grid container spacing={2} className='pr-24'>
           <Grid item xs={12}>
             <InputLabel className='font-poppins mb-1'>Name</InputLabel>
@@ -22,10 +22,10 @@ function RegistrationBox() {
           </Grid>
           <Grid item xs={12}>
             <InputLabel>Department</InputLabel>
-            <Select options={Departments} isClearable className='mt-1 landing-select h-12 text-md w-full rounded border-1 border-black/[.15]' placeholder='Click here'/>
+            <Select options={Departments} isClearable className='landing-select' placeholder='Click here'/>
           </Grid>
           <Grid item xs={12} className='mt-12 flex flex-col align-left'>
-            <Button type='button' variant='outlined' className='w-fit normal-case text-lg font-bold bg-secondary text-white py-2 px-4'>Submit</Button>
+            <Button type='button' variant='contained' className='landing-registration-button'>Register</Button>
           </Grid>
         </Grid>
     </Container>
