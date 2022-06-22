c5a6d0caa58c92ed8c936ec91bc7a23ae6014e68
kailash360 <kailashkejriwal21@gmail.com>
2022-06-23 02:24:19 +0530

    fixed landing page

--- frontend/src/pages/Landing.jsx
+++ frontend/src/pages/Landing.jsx
@@ -13,29 +13,29 @@ function Home() {
   const setAdmin = () => setRole('admin')
 
   return (
-    <Grid container overflow-hidden	>
-      <Grid item lg={6} className='min-h-full pl-24 min-h-screen'>
+    <Grid container overflow-hidden	className='landing'>
+      <Grid item lg={6} className='landing-left'>
       {!role && 
       <>
-        <p className='text-6xl text-secondary mx-2 font-bold mb-1'> DeDocs </p>
-        <p className='text-4xl text-secondary mx-2 font-semibold'>
+        <p className='landing-left-heading'> DeDocs </p>
+        <p className='landing-left-subheading'>
           A Decentralized <br /> 
           Document Management <br /> 
           System 
         </p>
-        <p className='max-w-lg mx-2'>
+        <p className='landing-left-description'>
           Lorem ipsum dolor sit amet consectetur adipisicing elit. Saepe iure laboriosam inventore quisquam omnis quam, dolore quaerat in assumenda dolorem illo aperiam sit aliquam architecto facere adipisci enim incidunt pariatur.
         </p>
-        <Container maxWidth={false} className="pl-0 flex-row justify-between mt-16">
-          <Button className="bg-secondary text-white normal-case px-8 py-2 text-lg ml-2 mr-8 my-2	" onClick={setUser}>I am a User</Button>
-          <Button className="bg-tertiary text-white normal-case	px-8 py-2 text-lg	" onClick={setAdmin}>I am an Admin</Button>
+        <Container maxWidth={false} className="landing-left-buttonContainer">
+          <Button className="landing-left-button user" variant='contained' onClick={setUser}>I am a User</Button>
+          <Button className="landing-left-button admin" variant='contained' onClick={setAdmin}>I am an Admin</Button>
         </Container>
       </>}
       {role == 'user' && <UserRegisterationBox/>}
       {role == 'admin' && <AdminRegisterationBox/>}
       </Grid>
-      <Grid item lg={6} className='bg-tertiary min-h-screen'>
-        <LandingVector className="my-[20%]"></LandingVector>
+      <Grid item lg={6} className='landing-right'>
+        <LandingVector className="landing-right-image"></LandingVector>
       </Grid>
     </Grid>
   )
