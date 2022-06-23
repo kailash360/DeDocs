563e4a73845cae4217d601a3db7c696f1f526abd
kailash360 <kailashkejriwal21@gmail.com>
2022-06-23 11:41:18 +0530

    fixed user dashboard

--- frontend/src/pages/User/Dashboard.jsx
+++ frontend/src/pages/User/Dashboard.jsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import {Container, Grid,IconButton} from '@mui/material'
 import ContentCopyIcon from '@mui/icons-material/ContentCopy';
+import '../../static/scss/User/Dashboard.scss'
 
 function Dashboard() {
 
@@ -8,18 +9,18 @@ function Dashboard() {
   const [address, setAddress] = React.useState('0x123456789qwerty')
 
   return (
-    <Container>
+    <Container className='dashboard'>
       <Grid container>
-        <Grid item>
-          <p className='mb-0 text-3xl font-medium'>
+        <Grid item className='dashboard-top'>
+          <p className='dashboard-top-message'>
             Welcome 
-            <span className='ml-2 font-bold text-tertiary'>{name}</span> 
+            <span className='dashboard-top-message-name'>{name}</span> 
           </p>
-          <p className='mt-1 text-lg'>
-            Address: 
-            <span className='ml-1 font-bold text-tertiary'>{address}</span> 
-            <IconButton aria-label='copy'>
-              <ContentCopyIcon className='text-[80%] text-black'/>
+          <p className='dashboard-top-address'>
+            Address &nbsp;
+            <span className='dashboard-top-address-value'>{address}</span> 
+            <IconButton aria-label='copy' type='button' title='Copy Address'>
+              <ContentCopyIcon className='dashboard-top-address-button'/>
             </IconButton> 
           </p>
         </Grid>
