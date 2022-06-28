2a41c925d4a5131be3b0bbfa12057f3f65e7bd9e
kailash360 <kailashkejriwal21@gmail.com>
2022-06-29 03:27:30 +0530

    added num conversion

--- frontend/src/utils/ConvertDate.js
+++ frontend/src/utils/ConvertDate.js
@@ -1,5 +1,5 @@
 const convertDate = (_date) => {
-    const date = new Date(_date)
+    const date = new Date(Number(_date))
     return date.toLocaleString('en-US', { year: 'numeric', month: 'long', day: 'numeric' })
 }
 
