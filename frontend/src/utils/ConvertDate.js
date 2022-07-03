3857f0762b7215960e7ef0dd92af84572ebd0d27
kailash360 <kailashkejriwal21@gmail.com>
2022-07-04 03:36:51 +0530

    modified date format

--- frontend/src/utils/ConvertDate.js
+++ frontend/src/utils/ConvertDate.js
@@ -1,6 +1,6 @@
 const convertDate = (_date) => {
     const date = new Date(Number(_date))
-    return date.toLocaleString('en-US', { year: 'numeric', month: 'long', day: 'numeric' })
+    return date.toLocaleString('en-US', { year: 'numeric', month: 'long', day: 'numeric' }) + '\t\t' + date.toLocaleTimeString()
 }
 
 export default convertDate
